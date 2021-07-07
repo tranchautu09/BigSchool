98b21e149e3125d71ef0ce08eeef8207825a0a4b
tranchautu09 <76834166+tranchautu09@users.noreply.github.com>
2021-07-07 23:17:28 +0700

    up

---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,46 @@
+﻿using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        public ActionResult Create()
+        {
+            BigSchoolContext context = new BigSchoolContext();
+            Course objCourse = new Course();
+            objCourse.ListCategory = context.Category.ToList();
+            return View(objCourse);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Course objCourse)
+        {
+            BigSchoolContext context = new BigSchoolContext();
+
+            ModelState.Remove("LecturerId");
+            if (!ModelState.IsValid)
+            {
+                objCourse.ListCategory = context.Category.ToList();
+                return View("Create", objCourse);
+            }
+
+            ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            objCourse.LecturerId = user.Id;
+
+            context.Course.Add(objCourse);
+            context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Models/Course.cs
@@ -0,0 +1,29 @@
+namespace BigSchool.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Course")]
+    public partial class Course
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        public string LecturerId { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Place { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public int CategoryId { get; set; }
+        public String Name;
+        public virtual Category Category { get; set; }
+        public List<Category> ListCategory = new List<Category>();
+    }
+}
